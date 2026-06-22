66d0e2470eb610c55a7b642969b4c48b406e6cf3
MYSQASEM <alsalahe@yahoo.com>
2026-06-22 18:26:29 +0300

    Delete SpeedLimiter-App-main (4)/SpeedLimiter-App-main directory

--- SpeedLimiter-App-main (4)/SpeedLimiter-App-main/app/src/main/java/com/qasim/speedlimiter/AppConfig.kt
+++
@@ -1,8 +0,0 @@
-package com.qasim.speedlimiter
-
-object AppConfig {
-    const val DEFAULT_SPEED_LIMIT = 80
-    const val PREFS_NAME = "SpeedLimiterPrefs"
-    const val KEY_SPEED_LIMIT = "speed_limit"
-    const val KEY_IS_ENABLED = "is_enabled"
-}
